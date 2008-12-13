ec8b64d46038fc5edfbfbd417a59b378ad9184af
Guillermo Álvarez <guillermo@cientifico.net>
2008-12-13 18:59:34 +0100

    Creado controlador links

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,11 @@
+class LinksController
+  include Mack::Controller
+  
+  # '/'
+  # Note: You do not need to actually have an empty action like this defined for Mack to
+  # find the view on disk. This is only included here for the sake of 'completeness'.
+  def index
+    
+  end
+
+end
