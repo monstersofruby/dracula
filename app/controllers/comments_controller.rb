2ef595e249cf43273dbba883f498a91f1fea65b8
Guillermo Álvarez <guillermo@cientifico.net>
2008-12-13 19:25:14 +0100

    Acciones para links con vistas

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController
   # '/'
   # Note: You do not need to actually have an empty action like this defined for Mack to
   # find the view on disk. This is only included here for the sake of 'completeness'.
-  def index
+  def index    
     
   end
 
